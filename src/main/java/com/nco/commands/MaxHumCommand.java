4149d1398269d0245d4f752ed8cf84e467face8a
Harry <harrycarr64@hotmail.com>
2021-05-27 17:04:55 +0100

    Factorised down and removed duplicate calculations

--- src/main/java/com/nco/commands/MaxHumCommand.java
+++ src/main/java/com/nco/commands/MaxHumCommand.java
@@ -33,25 +33,21 @@ public class MaxHumCommand extends AbstractCommand {
 
     @Override
     protected void processUpdateAndRespond(Connection conn, PlayerCharacter pc, EmbedBuilder builder) throws SQLException {
-        if (updateMaxHum(pc.getMaxHumanity(), conn) ) {
-            int oldMaxHum = pc.getMaxHumanity();
-            int newMaxHum = oldMaxHum + (messageArgs[1].equalsIgnoreCase("cyberware") ? 2 : 4);
-
+        int oldMaxHum = pc.getMaxHumanity();
+        int newMaxHum = oldMaxHum + (messageArgs[1].equalsIgnoreCase("cyberware") ? 2 : 4);
+        if (updateMaxHum(newMaxHum, conn) ) {
             builder.setTitle(messageArgs[0] + "'s MaxHumanity Updated");
             builder.setDescription("Removed \"" + messageArgs[1] + "\"");
             builder.addField("Old MaxHumanity", String.valueOf(oldMaxHum), true);
             builder.addBlankField(true);
             builder.addField("New MaxHumanity", String.valueOf(newMaxHum), true);
-
         } else {
             builder.setTitle("ERROR: MaxHum Update Or Insert Failure");
             builder.setDescription("Please contact an administrator to get this resolved");
         }
     }
 
-    private boolean updateMaxHum(int currentMaxHumanity, Connection conn) throws SQLException {
-        int newMaxHum = (messageArgs[1].equalsIgnoreCase("cyberware") ? 2 : 4);
-        int newMaxHumTotal = currentMaxHumanity + newMaxHum;
+    private boolean updateMaxHum(int newMaxHumTotal, Connection conn) throws SQLException {
         String sql = "UPDATE NCO_PC set max_humanity = ? Where character_name = ?";
         try (PreparedStatement stat = conn.prepareStatement(sql)) {
             stat.setInt(1, newMaxHumTotal);
