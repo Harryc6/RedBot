c4ec70d9f03d6f67d8a32cfb84fa35349f773774
Harry <harrycarr64@hotmail.com>
2021-05-27 17:53:53 +0100

    Added logging to MaxHumCommand

--- src/main/java/com/nco/commands/MaxHumCommand.java
+++ src/main/java/com/nco/commands/MaxHumCommand.java
@@ -35,7 +35,7 @@ public class MaxHumCommand extends AbstractCommand {
     protected void processUpdateAndRespond(Connection conn, PlayerCharacter pc, EmbedBuilder builder) throws SQLException {
         int oldMaxHum = pc.getMaxHumanity();
         int newMaxHum = oldMaxHum + (messageArgs[1].equalsIgnoreCase("cyberware") ? 2 : 4);
-        if (updateMaxHum(newMaxHum, conn) ) {
+        if (updateMaxHum(newMaxHum, conn) && insertMaxHum(conn, oldMaxHum, newMaxHum)) {
             builder.setTitle(messageArgs[0] + "'s MaxHumanity Updated");
             builder.setDescription("Removed \"" + messageArgs[1] + "\"");
             builder.addField("Old MaxHumanity", String.valueOf(oldMaxHum), true);
@@ -52,6 +52,19 @@ public class MaxHumCommand extends AbstractCommand {
         try (PreparedStatement stat = conn.prepareStatement(sql)) {
             stat.setInt(1, newMaxHumTotal);
             stat.setString(2, messageArgs[0]);
+            return stat.executeUpdate() == 1;
+        }
+    }
+
+    private boolean insertMaxHum(Connection conn, int oldMaxHum, int newMaxHum) throws SQLException {
+        String sql = "insert into nco_max_humanity (character_name, cyber_or_borg, old_max_humanity, new_max_humanity, created_by) " +
+                "values (?, ?, ?, ?, ?)";
+        try (PreparedStatement stat = conn.prepareStatement(sql)) {
+            stat.setString(1, messageArgs[0]);
+            stat.setString(2, messageArgs[1]);
+            stat.setInt(3, oldMaxHum);
+            stat.setInt(4, newMaxHum);
+            stat.setString(5, author.getAsTag());
             return stat.executeUpdate() == 1;
         }
     }
