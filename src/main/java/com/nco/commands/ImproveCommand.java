fe260e510ccde2e216e3271ff7ebb61711ae2ff5
Harry <harrycarr64@hotmail.com>
2021-06-02 16:25:51 +0100

    Updated improve validation and formatting

--- src/main/java/com/nco/commands/ImproveCommand.java
+++ src/main/java/com/nco/commands/ImproveCommand.java
@@ -2,6 +2,7 @@ package com.nco.commands;
 
 import com.nco.RedBot;
 import com.nco.pojos.PlayerCharacter;
+import com.nco.utils.NumberUtils;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.MessageChannel;
@@ -19,42 +20,33 @@ public class ImproveCommand extends AbstractCommand {
 
     @Override
     protected boolean canProcessByUser() {
-        return messageArgs.length == 2;
+        return messageArgs.length == 2 && NumberUtils.isNumeric(messageArgs[1]);
     }
 
     @Override
     protected boolean canProcessByName() {
-        return messageArgs.length == 3;
+        return messageArgs.length == 3 && NumberUtils.isNumeric(messageArgs[2]);
     }
 
 
 
     @Override
     protected void processUpdateAndRespond(Connection conn, PlayerCharacter pc, EmbedBuilder builder) throws SQLException {
-        if (updateImprove(pc, conn) && insertImprove(conn)) {
-            int changeIP = Integer.parseInt(messageArgs[2]);
-            int oldIP = pc.getInfluencePoints();
-            int newIP = oldIP - (changeIP < 0 ? -changeIP : changeIP);
-
-            builder.setTitle(messageArgs[0] + "'s IP Updated");
-            builder.setDescription("For \"" + messageArgs[1] + "\"");
-            builder.addField("Old IP", String.valueOf(oldIP), true);
-            builder.addBlankField(true);
-            builder.addField("New IP", String.valueOf(newIP), true);
-
+        int newIP = pc.getInfluencePoints() - NumberUtils.asPositive(messageArgs[2]);
+        if (newIP < 0) {
+            builder.setTitle("ERROR: Not Enough IP");
+            builder.setDescription(messageArgs[0] + " has only " + pc.getInfluencePoints() + " IP available " +
+                    "where " + NumberUtils.asPositive(messageArgs[2]) + " IP is being used.");
+        } else if (updateImprove(newIP, conn) && insertImprove(conn)) {
+            buildEmbeddedContent(pc, builder, newIP);
         } else {
             builder.setTitle("ERROR: Improve Update Or Insert Failure");
             builder.setDescription("Please contact an administrator to get this resolved");
         }
     }
 
-    private boolean updateImprove(PlayerCharacter pc, Connection conn) throws SQLException {
-        int changeIP = Integer.parseInt(messageArgs[2]);
-        int oldIP = pc.getInfluencePoints();
-        int newIP = oldIP - (changeIP < 0 ? -changeIP : changeIP);
-
+    private boolean updateImprove(int newIP, Connection conn) throws SQLException {
         String sql = "UPDATE NCO_PC set influence_points = ? Where character_name = ?";
-
         try (PreparedStatement stat = conn.prepareStatement(sql)) {
             stat.setInt(1, newIP);
             stat.setString(2, messageArgs[0]);
@@ -71,6 +63,14 @@ public class ImproveCommand extends AbstractCommand {
             stat.setString(4, author.getAsTag());
             return stat.executeUpdate() == 1;
         }
+    }
+
+    private void buildEmbeddedContent(PlayerCharacter pc, EmbedBuilder builder, int newIP) {
+        builder.setTitle(messageArgs[0] + "'s IP Updated");
+        builder.setDescription("For \"" + messageArgs[1] + "\"");
+        builder.addField("Old IP", String.valueOf(pc.getInfluencePoints()), true);
+        builder.addBlankField(true);
+        builder.addField("New IP", String.valueOf(newIP), true);
     }
 
     @Override
